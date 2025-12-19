0a62a382c504c5864c1c54ff868658ef21a14246
mehernigar17 <mehernigar1744@gmail.com>
2025-12-19 20:17:51 +0600

    Upload Commit

---
+++ sort_only_at_even_indices.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> vec;
+int main(){
+int n,x;
+cin>>n;
+for(int i=0;i<n;i++){
+    cin>>x;
+    vec.push_back(x);
+}
+
+
+for(int i=0;i<=n-2;i++){
+    if(i%2!=0)continue;
+        int minIndex=i;
+    for(int j=i+1;j<=n-1;j++){
+        if(j%2==0&&vec[j]<vec[minIndex]){
+            minIndex=j;
+        }
+    }
+    swap(vec[i],vec[minIndex]);
+}
+
+for(int i=0;i<n;i++){
+    cout<<vec[i]<<endl;
+}
+
+
+
+return 0;}
