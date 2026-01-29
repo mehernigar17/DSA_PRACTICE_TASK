7341836f63ddc72282f1480e4dd88bb1947cdccd
mehernigar17 <mehernigar1744@gmail.com>
2026-01-29 12:57:27 +0600

    upload commit

---
+++ doubly_insert_at_end.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+
+int data;
+struct node* prev;
+struct node *next;
+};
+struct node *head=NULL;
+void insert_at_end(int data){
+
+struct node *newnode=malloc(sizeof(struct node));
+newnode->data = data;
+
+if(head==NULL){
+    newnode->prev=NULL;
+    newnode->next = NULL;
+
+    head=newnode;
+
+}
+else{
+    struct node*ptr=head;
+    while(ptr->next!=NULL){
+        ptr=ptr->next;
+    }
+    ptr->next=newnode;
+    newnode->prev=ptr;
+    newnode->next=NULL;
+}
+
+}
+
+void traverse(){
+
+struct  node*ptr;
+
+for(ptr=head;ptr!=NULL;ptr=ptr->next){
+    printf("%d ",ptr->data);
+}
+
+}
+
+int main(){
+
+
+  int n,value;
+    printf ("number of nodes");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++)
+    {
+
+        scanf("%d",&value);
+          insert_at_end(value);
+
+
+
+    }
+ traverse();
+return 0;
+}
