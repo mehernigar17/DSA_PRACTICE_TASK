b15c67b4116ebb03174d305ba094e9eef794ce11
mehernigar17 <mehernigar1744@gmail.com>
2026-06-30 23:40:38 +0600

    upload

---
+++ short_distance_btn_source_and-distance.cpp
@@ -0,0 +1,75 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> graph[1000];
+bool visited[1000];
+int dist[1000];
+
+void BFS(int source)
+{
+    queue<int> q;
+
+    visited[source] = true;
+    dist[source] = 0;
+    q.push(source);
+
+    while(!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+
+        for(int i = 0; i < graph[node].size(); i++)
+        {
+            int neighbor = graph[node][i];
+
+            if(!visited[neighbor])
+            {
+                visited[neighbor] = true;
+                dist[neighbor] = dist[node] + 1;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    for(int i = 0; i < e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        graph[u].push_back(v);
+        graph[v].push_back(u);   // Undirected
+    }
+
+    int source, destination;
+    cin >> source >> destination;
+
+    for(int i = 0; i < n; i++)
+    {
+        visited[i] = false;
+        dist[i] = -1;
+    }
+
+    BFS(source);
+
+    if(dist[destination] == -1)
+    {
+        cout << "No path exists" << endl;
+    }
+    else
+    {
+        cout << "Shortest Distance = "
+             << dist[destination] << endl;
+
+        cout << "Cost = "
+             << dist[destination] << endl;
+    }
+
+    return 0;
+}
